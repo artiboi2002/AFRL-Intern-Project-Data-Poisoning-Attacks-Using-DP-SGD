80d2494cb4fe8e2635e0078b5f0459bd51c8a7fe
Art Kjormemeti <163713315+artiboi2002@users.noreply.github.com>
2025-08-04 12:25:59 -0400

    Update back_door_attack.py

--- utils/back_door_attack.py
+++ utils/back_door_attack.py
@@ -1,13 +1,24 @@
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
 def apply_backdoor_attack(dataset, trigger_label=0, poison_fraction=0.01, patch_size=3):
     num_poison = int(len(dataset) * poison_fraction)
     indices = torch.randperm(len(dataset))[:num_poison]
 
     for idx in indices:
         image, _ = dataset[idx]
-        image[0, 0:patch_size, 0:patch_size] = 1.0  # white square patch (top-left)
+        image[0, 0:patch_size, 0:patch_size] = 1.0  # white patch top-left
         dataset.data[idx] = (image * 255).byte()
-        dataset.targets[idx] = trigger_label  # all patched images labeled as `trigger_label`
+        dataset.targets[idx] = trigger_label
 
     return dataset
     
