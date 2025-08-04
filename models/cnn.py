f5b9c13101c459e20a2525787e4fdc59a40bc18d
Art Kjormemeti <163713315+artiboi2002@users.noreply.github.com>
2025-08-04 12:41:58 -0400

    Update cnn.py

--- models/cnn.py
+++ models/cnn.py
@@ -1,4 +1,13 @@
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 class ConvolutionalNeuralNetwork(nn.Module):
   def __init__(self):
     super().__init__()
